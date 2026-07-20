fb75e35b63af8722727e1db7ff5a96f2a2237772
Դավիթ Տեր-Պետրոսյան <145901808+DavitPetrosyaann@users.noreply.github.com>
2026-07-20 11:42:43 +0400

    Add files via upload

---
+++ Python Դասեր/Դաս 1/lesson_1.py
@@ -0,0 +1,16 @@
+# open termianl - Ctrl + ``
+# terminal - dir - show your files
+# cd - change directory
+# python <file name> - run your file
+# or select code and do - <Ctrl + Alt + N>
+x = 10
+y = 15
+print(x + y)
+if x < 10:
+    print("SHalala")
+elif x == 10:
+    print("100000")
+else:
+    print("asdasdasd")
+for i in range(1, 10):
+    print("asd")
